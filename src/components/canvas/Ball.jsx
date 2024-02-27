ff2ef52d21e90bc76578e9a12a71e47882f5e8a8
akash-charles <akash262003@gmail.com>
2024-02-27 12:38:45 +0530

    more updates

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -5,7 +5,7 @@ import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/d
 import CanvasLoader from '../Loader';
 
 const Ball = ({ imgUrl }) => {
-  const [decal] = useTexture([props.imgUrl]);
+  const [decal] = useTexture([imgUrl]);
 
   return (
     <Float speed={1.78} rotationIntensity={1} floatIntensity={2}>
@@ -14,7 +14,7 @@ const Ball = ({ imgUrl }) => {
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial color='#fff8eb' polygonOffset polygonOffsetFactor={-5} flatShading />
-        <Decal position={[0, 0, 1]} map={decal} />
+        <Decal position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.25]} map={decal} />
       </mesh>
     </Float>
   )
